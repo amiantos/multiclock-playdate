a625dd05ed009920056463b6a8cc6eb766768383
Brad Root <bradroot@me.com>
2022-05-19 22:21:24 -0700

    initial commit

---
+++ source/main.lua
@@ -0,0 +1,48 @@
+-- Name this file `main.lua`. Your game can use multiple source files if you wish
+-- (use the `import "myFilename"` command), but the simplest games can be written
+-- with just `main.lua`.
+
+-- You'll want to import these in just about every project you'll work on.
+
+import "CoreLibs/object"
+import "CoreLibs/graphics"
+import "CoreLibs/sprites"
+import "CoreLibs/timer"
+
+-- Declaring this "gfx" shorthand will make your life easier. Instead of having
+-- to preface all graphics calls with "playdate.graphics", just use "gfx."
+-- Performance will be slightly enhanced, too.
+-- NOTE: Because it's local, you'll have to do it in every .lua source file.
+
+local gfx <const> = playdate.graphics
+
+-- Here's our player sprite declaration. We'll scope it to this file because
+-- several functions need to access it.
+
+
+-- A function to set up our game environment.
+
+function myGameSetUp()
+
+
+
+end
+
+-- Now we'll call the function above to configure our game.
+-- After this runs (it just runs once), nearly everything will be
+-- controlled by the OS calling `playdate.update()` 30 times a second.
+
+myGameSetUp()
+
+-- `playdate.update()` is the heart of every Playdate game.
+-- This function is called right before every frame is drawn onscreen.
+-- Use this function to poll input, run game logic, and move sprites.
+
+function playdate.update()
+
+	
+	
+	gfx.sprite.update()
+	playdate.timer.updateTimers()
+
+end
